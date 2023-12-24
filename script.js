b3dd7aacf7b3292540ec4dc74b5705cc6733684e
yKXuan1612 <119069282+yKXuan1612@users.noreply.github.com>
2023-12-25 01:31:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,56 @@
-function loadXMLContent(url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(data, "text/xml");
+// embed the xml file into script
+const studentInfo = {
+    MSSV: 'K214110802',
+    Tên: 'Ma Ngọc Xuân Kỳ',
+    Gender: 'Nam',
+    Lớp: '21411',
+    SĐT: '0123456789',
+    Email: 'kymnx21411@st.uel.edu.vn',
+    Country: 'Nam Định',
+    Hobby: 'Nghe nhạc, đá cầu, cầu lông, chơi game, ngắm cảnh, dạo cà phê, v.v'
+};
 
-            const contentDiv = document.getElementById('content');
-            contentDiv.innerHTML = ''; // Clear current content
+// update the function
 
-            const studentInfo = xmlDoc.getElementsByTagName('student')[0];
-            const attributes = studentInfo.children;
+function loadStudentInfo(studentInfo) {
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = ''; // Clear current content
 
-            // Create a table element
-            const table = document.createElement('table');
-            table.className = 'xml-student-table';
+    // Create a table element
+    const table = document.createElement('table');
+    table.className = 'xml-student-table';
 
-            // Create table header
-            const headerRow = table.createTHead().insertRow(0);
-            headerRow.className = 'xml-student-header-row';
-            const th1 = document.createElement('th');
-            th1.textContent = 'Thông tin';
-            headerRow.appendChild(th1);
-            const th2 = document.createElement('th');
-            th2.textContent = 'Chi tiết';
-            headerRow.appendChild(th2);
+    // Create table header
+    const headerRow = table.createTHead().insertRow(0);
+    headerRow.className = 'xml-student-header-row';
+    const th1 = document.createElement('th');
+    th1.textContent = 'Thông tin';
+    headerRow.appendChild(th1);
+    const th2 = document.createElement('th');
+    th2.textContent = 'Chi tiết';
+    headerRow.appendChild(th2);
 
-            // Create table body
-            const body = table.createTBody();
-            for (const attribute of attributes) {
-                const dataRow = body.insertRow();
-                dataRow.className = 'xml-student-data-row';
-                const td1 = document.createElement('td');
-                td1.textContent = attribute.tagName;
-                dataRow.appendChild(td1);
-                const td2 = document.createElement('td');
-                td2.textContent = attribute.textContent;
-                dataRow.appendChild(td2);
-            }
+    // Create table body
+    const body = table.createTBody();
+    for (const attribute in studentInfo) {
+        const dataRow = body.insertRow();
+        dataRow.className = 'xml-student-data-row';
+        const td1 = document.createElement('td');
+        td1.textContent = attribute;
+        dataRow.appendChild(td1);
+        const td2 = document.createElement('td');
+        td2.textContent = studentInfo[attribute];
+        dataRow.appendChild(td2);
+    }
 
-            // Append the table to the contentDiv
-            contentDiv.appendChild(table);
-        })
-        .catch(error => {
-            console.error('Error loading XML content:', error);
-        });
-}
-
-function loadSubMenuContent(url, targetId) {
-    console.log('Loading content from:', url);
-
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            const contentDiv = document.getElementById(targetId);
-
-            if (!contentDiv) {
-                console.error(`Target element with id '${targetId}' not found.`);
-                return;
-            }
-
-            // Create a temporary container and set its innerHTML to the fetched data
-            const tempContainer = document.createElement('div');
-            tempContainer.innerHTML = data;
-
-            // Select the specific section you want to append to the contentDiv
-            const sectionC = tempContainer.querySelector('.section-c');
-
-            if (sectionC) {
-                // Clear the current content of contentDiv
-                contentDiv.innerHTML = '';
-
-                // Append the selected section to the contentDiv
-                contentDiv.appendChild(sectionC);
-            } else {
-                console.error('Section-c not found in the loaded content.');
-            }
-        })
-        .catch(error => {
-            console.error('Error loading content:', error);
-        });
+    // Append the table to the contentDiv
+    contentDiv.appendChild(table);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     // Load student info by default
     if (window.location.pathname.endsWith('index.html')) {
-        loadXMLContent('K214110802.xml');
+        loadStudentInfo(studentInfo);
     }
 
     // Add event listeners to menu items
@@ -104,6 +70,115 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// Local script to read XML
+
+// function loadXMLContent(url) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             const parser = new DOMParser();
+//             const xmlDoc = parser.parseFromString(data, "text/xml");
+
+//             const contentDiv = document.getElementById('content');
+//             contentDiv.innerHTML = ''; // Clear current content
+
+//             const studentInfo = xmlDoc.getElementsByTagName('student')[0];
+//             const attributes = studentInfo.children;
+
+//             // Create a table element
+//             const table = document.createElement('table');
+//             table.className = 'xml-student-table';
+
+//             // Create table header
+//             const headerRow = table.createTHead().insertRow(0);
+//             headerRow.className = 'xml-student-header-row';
+//             const th1 = document.createElement('th');
+//             th1.textContent = 'Thông tin';
+//             headerRow.appendChild(th1);
+//             const th2 = document.createElement('th');
+//             th2.textContent = 'Chi tiết';
+//             headerRow.appendChild(th2);
+
+//             // Create table body
+//             const body = table.createTBody();
+//             for (const attribute of attributes) {
+//                 const dataRow = body.insertRow();
+//                 dataRow.className = 'xml-student-data-row';
+//                 const td1 = document.createElement('td');
+//                 td1.textContent = attribute.tagName;
+//                 dataRow.appendChild(td1);
+//                 const td2 = document.createElement('td');
+//                 td2.textContent = attribute.textContent;
+//                 dataRow.appendChild(td2);
+//             }
+
+//             // Append the table to the contentDiv
+//             contentDiv.appendChild(table);
+//         })
+//         .catch(error => {
+//             console.error('Error loading XML content:', error);
+//         });
+// }
+
+// function loadSubMenuContent(url, targetId) {
+//     console.log('Loading content from:', url);
+
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             const contentDiv = document.getElementById(targetId);
+
+//             if (!contentDiv) {
+//                 console.error(`Target element with id '${targetId}' not found.`);
+//                 return;
+//             }
+
+//             // Create a temporary container and set its innerHTML to the fetched data
+//             const tempContainer = document.createElement('div');
+//             tempContainer.innerHTML = data;
+
+//             // Select the specific section you want to append to the contentDiv
+//             const sectionC = tempContainer.querySelector('.section-c');
+
+//             if (sectionC) {
+//                 // Clear the current content of contentDiv
+//                 contentDiv.innerHTML = '';
+
+//                 // Append the selected section to the contentDiv
+//                 contentDiv.appendChild(sectionC);
+//             } else {
+//                 console.error('Section-c not found in the loaded content.');
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error loading content:', error);
+//         });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Load student info by default
+//     if (window.location.pathname.endsWith('index.html')) {
+//         loadXMLContent('K214110802.xml');
+//     }
+
+//     // Add event listeners to menu items
+//     document.querySelectorAll('.link a').forEach(link => {
+//         link.addEventListener('click', function(event) {
+//             const contentDiv = document.getElementById('content');
+
+//             if (this.innerHTML === 'Trang Chủ') {
+//                 window.location.reload();
+//             } else if (this.innerHTML === 'Giới thiệu sách') {
+//                 // Navigate to the link directly in the current tab
+//                 window.location.href = this.href;
+//             } else if (this.innerHTML === 'Đăng ký') {
+//                 event.preventDefault();
+//                 loadSubMenuContent('register.html', 'content');
+//             }
+//         });
+//     });
+// });
 
 function submitForm(event) {
     event.preventDefault();
